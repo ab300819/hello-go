c317a1e24a6d0c23e395bce300fae0bb7d0641c8
Mason <ab300819@163.com>
2022-06-25 21:52:59 +0800

    feat: 添加go实战例子

--- go_geektime/demo02.go
+++ go_geektime/demo02.go
@@ -3,14 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 var name string
 
 func init() {
-	flag.StringVar(&name, "name", "every", "The greeting object.")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&name, "name", "everyone", "The greeting object.")
 }
 
 func main() {
+	flag.Parse()
 	fmt.Printf("hello, %s!\n", name)
 }
